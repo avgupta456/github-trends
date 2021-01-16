4956a5a28e3f3a41edd16d4220a8daf86d2dd51a
Abhijit Gupta <avgupta456@gmail.com>
2021-01-16 18:28:24 -0500

    use templates

--- backend/external/github_api/graphql/repo.py
+++ backend/external/github_api/graphql/repo.py
@@ -1,34 +1,42 @@
-"""
-query { 
-  repository(owner: "facebook", name: "react") { 
-    createdAt,
-    updatedAt,
-    forkCount,
-    forks(first: 10){
-    	nodes{
-        createdAt,
-      },
-    },
-    stargazerCount,
-    stargazers(first: 10){
-      nodes{
-        createdAt,
-      },
-    },
-    primaryLanguage{
-      name
-    },
-    languages(first: 5){
-      totalCount,
-      totalSize,
-      edges{
-        node {
-          name,
-        },
-        size,
-      },
-    },
-    
-  }
-}
-"""
+from external.github_api.graphql.template import get_template
+
+
+def get_repo(owner: str, repo: str) -> dict:
+    query = {
+        "variables": {"owner": owner, "repo": repo},
+        "query": """
+        query getRepo($owner: String!, $repo: String!) { 
+            repository(owner: $owner, name: $repo) { 
+                createdAt,
+                updatedAt,
+                forkCount,
+                forks(first: 10){
+                    nodes{
+                        createdAt,
+                    },
+                },
+                stargazerCount,
+                stargazers(first: 10){
+                    nodes{
+                        createdAt,
+                    },
+                },
+                primaryLanguage{
+                    name
+                },
+                languages(first: 5){
+                    totalCount,
+                    totalSize,
+                    edges{
+                        node {
+                            name,
+                        },
+                        size,
+                    },
+                },
+            }
+        }
+        """,
+    }
+
+    return get_template(query)
--- backend/external/github_api/graphql/user.py
+++ backend/external/github_api/graphql/user.py
@@ -1,135 +1,144 @@
-"""
-query { 
-  user(login: "avgupta456"){
-    contributionsCollection{
-      commitContributionsByRepository(maxRepositories: 10){
-        repository{
-          name,
-        },
-        contributions(first: 10){
-          totalCount
-          nodes{
-            commitCount,
-            occurredAt,       	
-          }
-        }
-      },
-      contributionCalendar{
-        totalContributions,
-        weeks{
-          contributionDays{
-            date,
-            weekday,
-            contributionCount,
-            contributionLevel,
-          }
-        }
-        colors,
-      }
-      contributionYears,
-      issueContributions(first: 10){
-        totalCount,
-        nodes{
-          occurredAt,
-          issue{
-            state
-          }
-        }
-      }
-      issueContributionsByRepository(maxRepositories: 10){
-        repository{
-          name
-        },
-        contributions(first: 10){
-          totalCount,
-          nodes{
-            occurredAt,
-            issue{
-              state
+from external.github_api.graphql.template import get_template
+
+
+def get_user(user_id: str) -> dict:
+    query = {
+        "variables": {"login": user_id},
+        "query": """
+        query getUser($login: String!) { 
+            user(login: $login){
+                contributionsCollection{
+                    commitContributionsByRepository(maxRepositories: 10){
+                        repository{
+                            name,
+                        },
+                        contributions(first: 10){
+                        totalCount
+                        nodes{
+                            commitCount,
+                            occurredAt,       	
+                        }
+                    }
+                },
+                contributionCalendar{
+                    totalContributions,
+                    weeks{
+                        contributionDays{
+                            date,
+                            weekday,
+                            contributionCount,
+                            contributionLevel,
+                        }
+                    }
+                    colors,
+                }
+                contributionYears,
+                issueContributions(first: 10){
+                    totalCount,
+                    nodes{
+                        occurredAt,
+                        issue{
+                            state
+                        }
+                    }
+                }
+                issueContributionsByRepository(maxRepositories: 10){
+                    repository{
+                        name
+                    },
+                    contributions(first: 10){
+                        totalCount,
+                        nodes{
+                            occurredAt,
+                            issue{
+                                state
+                            }
+                        }
+                    }
+                }
+                pullRequestContributions(first: 10){
+                    totalCount,
+                    nodes{
+                        occurredAt,
+                        pullRequest{
+                            state
+                        }
+                    }
+                }
+                pullRequestContributionsByRepository(maxRepositories:10){
+                    repository{
+                        name
+                    },
+                    contributions(first:10){
+                        totalCount,
+                        nodes{
+                            occurredAt,
+                            pullRequest{
+                                state,
+                            }
+                        }
+                    }
+                }
+                pullRequestReviewContributions(first: 10){
+                    totalCount,
+                    nodes{
+                        occurredAt,
+                        pullRequestReview{
+                            state
+                        }
+                    }
+                }
+                pullRequestReviewContributionsByRepository(maxRepositories:10){
+                    repository{
+                        name
+                    },
+                    contributions(first:10){
+                        totalCount,
+                        nodes{
+                            occurredAt,
+                            pullRequestReview{
+                                state,
+                            }
+                        }
+                    }
+                },
+                repositoryContributions(first:10){
+                    totalCount,
+                    nodes{
+                        repository{
+                            name,
+                        }
+                        occurredAt,
+                    }
+                },
+                restrictedContributionsCount,
+                totalCommitContributions,
+                totalIssueContributions,
+                totalPullRequestContributions,
+                totalPullRequestReviewContributions,
+                totalRepositoryContributions,
+                totalRepositoriesWithContributedCommits,
+                totalRepositoriesWithContributedIssues,
+                totalRepositoriesWithContributedPullRequests,
+                totalRepositoriesWithContributedPullRequestReviews
+                },
+                followers(first:10){
+                    totalCount,
+                    nodes{
+                        name,
+                        url,
+                    }
+                }
+                following(first:10){
+                    totalCount,
+                    nodes{
+                        name,
+                        url,
+                    }
+                }
             }
-          }
         }
-      }
-      pullRequestContributions(first: 10){
-        totalCount,
-        nodes{
-          occurredAt,
-          pullRequest{
-            state
-          }
-        }
-      }
-      pullRequestContributionsByRepository(maxRepositories:10){
-        repository{
-          name
-        },
-        contributions(first:10){
-          totalCount,
-          nodes{
-            occurredAt,
-            pullRequest{
-              state,
-            }
-          }
-        }
-      }
-      pullRequestReviewContributions(first: 10){
-        totalCount,
-        nodes{
-          occurredAt,
-          pullRequestReview{
-            state
-          }
-        }
-      }
-      pullRequestReviewContributionsByRepository(maxRepositories:10){
-        repository{
-          name
-        },
-        contributions(first:10){
-          totalCount,
-          nodes{
-            occurredAt,
-            pullRequestReview{
-              state,
-            }
-          }
-        }
-      },
-      repositoryContributions(first:10){
-        totalCount,
-        nodes{
-          repository{
-            name,
-          }
-          occurredAt,
-        }
-      },
-      restrictedContributionsCount,
-      totalCommitContributions,
-      totalIssueContributions,
-      totalPullRequestContributions,
-      totalPullRequestReviewContributions,
-      totalRepositoryContributions,
-      totalRepositoriesWithContributedCommits,
-      totalRepositoriesWithContributedIssues,
-      totalRepositoriesWithContributedPullRequests,
-      totalRepositoriesWithContributedPullRequestReviews
-    },
-    followers(first:10){
-      totalCount,
-      nodes{
-        name,
-        url,
-      }
+        """,
     }
-    following(first:10){
-      totalCount,
-      nodes{
-        name,
-        url,
-      }
-    }
-  }
-}
-"""
+
+    return get_template(query)
