83e083cea6ae9df13f1a73a7fce5335ddef4767b
Abhijit Gupta <avgupta456@gmail.com>
2021-01-18 18:35:14 -0500

    fail_gracefully decorator

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import logging
-from typing import List, Dict, Any
+
+from typing import Callable, List, Dict, Any
+from functools import wraps
 
 from fastapi import FastAPI, Response, status
 from dotenv import load_dotenv
@@ -19,23 +21,27 @@ def read_root():
     return {"Hello": "World"}
 
 
+def fail_gracefully(func: Callable[..., Any]):
+    @wraps(func)  # needed to play nice with FastAPI decorator
+    def wrapper(response: Response, *args: List[Any], **kwargs: Dict[str, Any]) -> Any:
+        try:
+            data = func(response, *args, **kwargs)
+            return {"data": data, "message": "200 OK"}
+        except Exception as e:
+            logging.exception(e)
+            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+            return {"data": [], "message": "Error " + str(e)}
+
+    return wrapper
+
+
 @app.get("/user/{user_id}", status_code=status.HTTP_200_OK)
-def get_user(user_id: str, response: Response) -> Dict[str, Any]:
-    try:
-        data: List[Dict[str, Any]] = list(map(lambda x: x.dict(), _get_user(user_id)))
-        return {"data": data, "message": ""}
-    except Exception as e:
-        logging.exception(e)
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {"data": [], "message": str(e)}
+@fail_gracefully
+def get_user(response: Response, user_id: str) -> List[Dict[str, Any]]:
+    return list(map(lambda x: x.dict(), _get_user(user_id)))
 
 
 @app.get("/repo/{user_id}/{repo_name}", status_code=status.HTTP_200_OK)
-def get_repo(user_id: str, repo_name: str, response: Response) -> Dict[str, Any]:
-    try:
-        data = _get_repo(user_id, repo_name)
-        return {"data": data, "message": ""}
-    except Exception as e:
-        logging.exception(e)
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {"data": [], "message": str(e)}
+@fail_gracefully
+def get_repo(response: Response, user_id: str, repo_name: str) -> Dict[str, Any]:
+    return _get_repo(user_id, repo_name)
