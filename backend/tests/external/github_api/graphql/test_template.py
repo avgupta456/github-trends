bc9884ec7393898e78475b57ba23e4fca61cf9a0
Abhijit Gupta <avgupta456@gmail.com>
2021-01-19 20:40:16 -0500

    create first test case

---
+++ backend/tests/external/github_api/graphql/test_template.py
@@ -0,0 +1,34 @@
+import unittest
+
+from external.github_api.graphql.template import get_template
+
+
+class TestTemplate(unittest.TestCase):
+    def test_get_template(self):
+        query = {
+            "variables": {"login": "avgupta456"},
+            "query": """
+            query getUser($login: String!) { 
+                user(login: $login){
+                    contributionsCollection{
+                        contributionCalendar{
+                            totalContributions
+                        }
+                    }
+                }
+            }
+            """,
+        }
+        response = get_template(query)
+
+        self.assertIn("data", response)
+        data = response["data"]
+        self.assertIn("user", data)
+        user = data["user"]
+        self.assertIn("contributionsCollection", user)
+        contributionsCollection = user["contributionsCollection"]
+        self.assertIn("contributionCalendar", contributionsCollection)
+        contributionCalendar = contributionsCollection["contributionCalendar"]
+        self.assertIn("totalContributions", contributionCalendar)
+        totalContributions = contributionCalendar["totalContributions"]
+        self.assertGreater(totalContributions, 0)
