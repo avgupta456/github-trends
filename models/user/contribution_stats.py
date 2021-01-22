809d1d7e58dff731c667375c037e687ea743686b
Abhijit Gupta <avgupta456@gmail.com>
2021-01-22 16:55:00 -0500

    simplify models

--- models/user/contribution_stats.py
+++ models/user/contribution_stats.py
@@ -1,5 +1,4 @@
-from enum import Enum
-from typing import List
+from typing import List, Optional
 
 # from models.misc.date import Date
 from pydantic import BaseModel, Field
@@ -8,37 +7,6 @@ from pydantic import BaseModel, Field
 """
 APIResponse
 """
-
-
-class IssueEnum(str, Enum):
-    """
-    Enum with options OPEN, CLOSED
-    """
-
-    OPEN = "OPEN"
-    CLOSED = "CLOSED"
-
-
-class PREnum(str, Enum):
-    """
-    Enum with options OPEN, MERGED, CLOSED
-    """
-
-    OPEN = "OPEN"
-    MERGED = "MERGED"
-    CLOSED = "CLOSED"
-
-
-class PRReviewEnum(str, Enum):
-    """
-    Enum with options PENDING, COMMENTED, CHANGES_REQUESTED, APPROVED, DISMISSED
-    """
-
-    PENDING = "PENDING"
-    COMMENTED = "COMMENTED"
-    CHANGES_REQUESTED = "CHANGES_REQUESTED"
-    APPROVED = "APPROVED"
-    DISMISSED = "DISMISSED"
 
 
 class APIResponse_Repo(BaseModel):
@@ -50,33 +18,6 @@ class APIResponse_Repo(BaseModel):
     name: str
 
 
-class APIResponse_IssueState(BaseModel):
-    """
-    BaseModel which accepts:
-    - state: IssueEnum
-    """
-
-    state: IssueEnum
-
-
-class APIResponse_PRState(BaseModel):
-    """
-    BaseModel which accepts:
-    - state: PREnum
-    """
-
-    state: PREnum
-
-
-class APIResponse_PRReviewState(BaseModel):
-    """
-    BaseModel which accepts:
-    - state: PRReviewEnum
-    """
-
-    state: PRReviewEnum
-
-
 class APIResponse_PageInfo(BaseModel):
     """
     BaseModel which accepts:
@@ -85,112 +26,43 @@ class APIResponse_PageInfo(BaseModel):
     """
 
     has_next_page: bool = Field(alias="hasNextPage")
-    end_cursor: str = Field(alias="endCursor")
+    end_cursor: Optional[str] = Field(alias="endCursor")
+
+    class Config:
+        allow_none = True
 
 
-class APIResponse_IssueContribsByRepo_Contrib(BaseModel):
+class APIResponse_Contrib(BaseModel):
     """
     BaseModel which accepts:
     - occurredAt: str
-    - issue: APIResponse_State
     """
 
     occurred_at: str = Field(alias="occurredAt")
-    issue: APIResponse_IssueState
 
 
-class APIResponse_IssueContribsByRepo_Contribs(BaseModel):
+class APIResponse_Contribs(BaseModel):
     """
     BaseModel which accepts:
     - totalCount: int
-    - nodes: List[APIResponse_IssueContribsByRepo_Contrib]
+    - nodes: List[APIResponse_Contrib]
     - pageInfo: APIResponse_PageInfo
     """
 
     total_count: int = Field(alias="totalCount")
-    nodes: List[APIResponse_IssueContribsByRepo_Contrib]
+    nodes: List[APIResponse_Contrib]
     page_info: APIResponse_PageInfo = Field(alias="pageInfo")
 
 
-class APIResponse_IssueContribsByRepo(BaseModel):
+class APIResponse_ContribsByRepo(BaseModel):
     """
     BaseModel which accepts:
     - repository: APIResponse_Repo
-    - contributions: APIResponse_IssueContribsByRepo_Contribs
+    - contributions: APIResponse_Contribs
     """
 
     repository: APIResponse_Repo
-    contributions: APIResponse_IssueContribsByRepo_Contribs
-
-
-class APIResponse_PRContribsByRepo_Contrib(BaseModel):
-    """
-    BaseModel which accepts:
-    - occurredAt: str
-    - pullRequest: APIResponse_State
-    """
-
-    occurred_at: str = Field(alias="occurredAt")
-    pull_request: APIResponse_PRState = Field(alias="pullRequest")
-
-
-class APIResponse_PRContribsByRepo_Contribs(BaseModel):
-    """
-    BaseModel which accepts:
-    - totalCount: int
-    - nodes: List[APIResponse_PRContribsByRepo_Contrib]
-    - pageInfo: APIResponse_PageInfo
-    """
-
-    total_count: int = Field(alias="totalCount")
-    nodes: List[APIResponse_PRContribsByRepo_Contrib]
-    page_info: APIResponse_PageInfo = Field(alias="pageInfo")
-
-
-class APIResponse_PRContribsByRepo(BaseModel):
-    """
-    BaseModel which accepts:
-    - repository: APIResponse_Repo
-    - contributions: APIResponse_PRContribsByRepo_Contribs
-    """
-
-    repository: APIResponse_Repo
-    contributions: APIResponse_PRContribsByRepo_Contribs
-
-
-class APIResponse_PRReviewContribsByRepo_Contrib(BaseModel):
-    """
-    BaseModel which accepts:
-    - occurredAt: str
-    - pullRequestReview: APIResponse_State
-    """
-
-    occurred_at: str = Field(alias="occurredAt")
-    pull_request_review: APIResponse_PRReviewState = Field(alias="pullRequestReview")
-
-
-class APIResponse_PRReviewContribsByRepo_Contribs(BaseModel):
-    """
-    BaseModel which accepts:
-    - totalCount: int
-    - nodes: List[APIResponse_PRReviewContribsByRepo_Contrib]
-    - pageInfo: APIResponse_PageInfo
-    """
-
-    total_count: int = Field(alias="totalCount")
-    nodes: List[APIResponse_PRReviewContribsByRepo_Contrib]
-    page_info: APIResponse_PageInfo = Field(alias="pageInfo")
-
-
-class APIResponse_PRReviewContribsByRepo(BaseModel):
-    """
-    BaseModel which accepts:
-    - repository: APIResponse_Repo
-    - contributions: APIResponse_PRReviewContribsByRepo_Contribs
-    """
-
-    repository: APIResponse_Repo
-    contributions: APIResponse_PRReviewContribsByRepo_Contribs
+    contributions: APIResponse_Contribs
 
 
 class APIResponse_RepoContribs_Contrib(BaseModel):
@@ -220,9 +92,9 @@ class APIResponse_RepoContribs(BaseModel):
 class APIResponse(BaseModel):
     """
     BaseModel which accepts:
-    - issueContributionsByRepository: List[APIResponse_IssueContribsByRepo]
-    - pullRequestContributionsByRepository: List[APIResponse_PRContribsByRepo]
-    - pullRequestReviewContributionsByRepository: List[APIResponse_PRReviewContribsByRepo]
+    - issueContributionsByRepository: List[APIResponse_ContribsByRepo]
+    - pullRequestContributionsByRepository: List[APIResponse_ContribsByRepo]
+    - pullRequestReviewContributionsByRepository: List[APIResponse_ContribsByRepo]
     - repositoryContributions: List[APIResponse_RepoContribs]
     - restrictedContributionsCount: int
     - totalIssueContributions: int
@@ -234,13 +106,13 @@ class APIResponse(BaseModel):
     - totalRepositoriesWithContributedPullRequestReviews: int
     """
 
-    issue_contribs_by_repo: List[APIResponse_IssueContribsByRepo] = Field(
+    issue_contribs_by_repo: List[APIResponse_ContribsByRepo] = Field(
         alias="issueContributionsByRepository"
     )
-    pr_contribs_by_repo: List[APIResponse_PRContribsByRepo] = Field(
+    pr_contribs_by_repo: List[APIResponse_ContribsByRepo] = Field(
         alias="pullRequestContributionsByRepository"
     )
-    pr_review_contribs_by_repo: List[APIResponse_PRReviewContribsByRepo] = Field(
+    pr_review_contribs_by_repo: List[APIResponse_ContribsByRepo] = Field(
         alias="pullRequestReviewContributionsByRepository"
     )
     repo_contribs: APIResponse_RepoContribs = Field(alias="repositoryContributions")
