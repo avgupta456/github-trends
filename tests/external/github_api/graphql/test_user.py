571e73f1afc11fc36f8ae008bbddaedb3dac0a46
Abhijit Gupta <avgupta456@gmail.com>
2021-01-22 22:25:00 -0500

    add more test cases

--- tests/external/github_api/graphql/test_user.py
+++ tests/external/github_api/graphql/test_user.py
@@ -28,20 +28,36 @@ class TestTemplate(unittest.TestCase):
 
     def test_get_user_contribution_commits(self):
         response = get_user_contribution_commits(user_id="avgupta456")
-
         self.assertIsInstance(response, UserContributionCommitsAPIResponse)
+
+        response = get_user_contribution_commits(user_id="avgupta456", max_repos=1)
+        self.assertLessEqual(len(response.commits_by_repo), 1)
+
+        response = get_user_contribution_commits(user_id="avgupta456", first=1)
+        self.assertLessEqual(len(response.commits_by_repo[0].contributions.nodes), 1)
 
     def test_get_user_contribution_stats(self):
         response = get_user_contribution_stats(user_id="avgupta456")
-
         self.assertIsInstance(response, UserContributionStatsAPIResponse)
+
+        response = get_user_contribution_stats(user_id="avgupta456", max_repos=1)
+        self.assertLessEqual(len(response.issue_contribs_by_repo), 1)
+
+        response = get_user_contribution_stats(user_id="avgupta456", first=1)
+        self.assertLessEqual(
+            len(response.issue_contribs_by_repo[0].contributions.nodes), 1
+        )
 
     def test_get_user_followers(self):
         response = get_user_followers(user_id="avgupta456")
-
         self.assertIsInstance(response, UserFollowAPIResponse)
+
+        response = get_user_followers(user_id="avgupta456", first=1)
+        self.assertLessEqual(len(response.nodes), 1)
 
     def test_get_user_following(self):
         response = get_user_following(user_id="avgupta456")
-
         self.assertIsInstance(response, UserFollowAPIResponse)
+
+        response = get_user_following(user_id="avgupta456", first=1)
+        self.assertLessEqual(len(response.nodes), 1)
