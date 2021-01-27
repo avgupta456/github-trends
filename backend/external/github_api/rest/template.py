2d2470814a3a1795e59db51f5e80b6ce21247782
Abhijit Gupta <avgupta456@gmail.com>
2021-01-26 23:07:38 -0500

    rest api authentication

--- backend/external/github_api/rest/template.py
+++ backend/external/github_api/rest/template.py
@@ -1,7 +1,9 @@
+import os
 from datetime import datetime
 from typing import Any, Dict
 
 import requests
+
 
 s = requests.session()
 
@@ -14,12 +16,19 @@ def get_template(
     query: str,
     plural: bool = False,
     per_page: int = 100,
+    page: int = 1,
     accept_header: str = "application/vnd.github.v3+json",
 ) -> Dict[str, Any]:
     """Template for interacting with the GitHub REST API"""
     start = datetime.now()
-    headers: Dict[str, str] = {"Accept": str(accept_header)}
-    params: Dict[str, str] = {"per_page": str(per_page)} if plural else {}
+    token = os.getenv("AUTH_TOKEN", "")
+    headers: Dict[str, str] = {
+        "Accept": str(accept_header),
+        "Authorization": "bearer " + token,
+    }
+    params: Dict[str, str] = (
+        {"per_page": str(per_page), "page": str(page)} if plural else {}
+    )
     r = s.get(query, params=params, headers=headers)  # type: ignore
     if r.status_code == 200:
         print("REST API", datetime.now() - start)
