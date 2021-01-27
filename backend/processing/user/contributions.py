2d33a7929df88e149f4fa19c4e7208381909b6db
Abhijit Gupta <avgupta456@gmail.com>
2021-01-26 23:09:01 -0500

    incorporate commit timestamps

--- backend/processing/user/contributions.py
+++ backend/processing/user/contributions.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
-from datetime import date, timedelta
-from pytz import timezone
+from datetime import date, datetime, timedelta
 from typing import Any, DefaultDict, Dict, List, Optional, Union
+
+import pytz
+from pytz import timezone
 
 from models.user.contribs import (
     RawCalendar,
@@ -16,19 +18,19 @@ from external.github_api.graphql.user import (
     get_user_contribution_events,
 )
 
+from external.github_api.rest.repo import get_repo_commits
+
 from helper.gather import gather
+
+from utils import date_to_datetime
 
 t = DefaultDict[str, Dict[str, List[Union[RawEventsEvent, RawEventsCommit]]]]
 
 
-# TODO: handle more timezones
-eastern = timezone("US/Eastern")
-
-
 def get_user_all_contribution_events(
     user_id: str,
-    start_date: date = date.today() - timedelta(365),
-    end_date: date = date.today(),
+    start_date: datetime = datetime.now() - timedelta(365),
+    end_date: datetime = datetime.now(),
 ) -> t:
     repo_contribs: t = defaultdict(
         lambda: {"commits": [], "issues": [], "prs": [], "reviews": [], "repos": []}
@@ -70,19 +72,67 @@ def get_user_all_contribution_events(
     return repo_contribs
 
 
+def get_all_commit_times(
+    user_id: str,
+    name_with_owner: str,
+    start_date: datetime = datetime.now(),
+    end_date: datetime = datetime.now(),
+) -> List[datetime]:
+    """Gets all user's commit times for a given repository"""
+    owner, repo = name_with_owner.split("/")
+    data, index = [], 0
+    while (index == 0 or (index > 0 and len(data) % 100 == 0)) and index < 10:
+        data.extend(
+            get_repo_commits(
+                owner=owner,
+                repo=repo,
+                user=user_id,
+                since=start_date,
+                until=end_date,
+                page=index + 1,
+            )
+        )
+        index += 1
+
+    data = list(
+        map(
+            lambda x: datetime.strptime(
+                x["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ"
+            ),
+            data,
+        )
+    )
+
+    # sort ascending
+    data = sorted(data)
+    return data
+
+
 def get_contributions(
     user_id: str,
     start_date: date = date.today() - timedelta(365),
     end_date: date = date.today(),
+    timezone_str: str = "US/Eastern",
 ) -> UserContributions:
+    tz = timezone(timezone_str)
 
     # get years for contribution calendar
-    years = list(
+    years = sorted(
         filter(
             lambda x: start_date.year <= x <= end_date.year,
             get_user_contribution_years(user_id),
         )
     )
+
+    dates = [
+        [
+            date_to_datetime(max(date(year, 1, 1), start_date)),
+            date_to_datetime(
+                min(date(year, 12, 31), end_date), hour=23, minute=59, second=59
+            ),
+        ]
+        for year in years
+    ]
 
     # async get contribution calendars
     calendars: List[RawCalendar] = gather(
@@ -90,32 +140,48 @@ def get_contributions(
         args_dicts=[
             {
                 "user_id": user_id,
-                "start_date": max(date(year, 1, 1), start_date),
-                "end_date": min(date(year, 12, 31), end_date),
+                "start_date": dates[i][0],
+                "end_date": dates[i][1],
             }
-            for year in years
+            for i in range(len(years))
         ],
     )
 
-    events: List[t] = gather(
+    all_events: List[t] = gather(
         funcs=[get_user_all_contribution_events for _ in years],
         args_dicts=[
             {
                 "user_id": user_id,
-                "start_date": max(date(year, 1, 1), start_date),
-                "end_date": min(date(year, 12, 31), end_date),
+                "start_date": dates[i][0],
+                "end_date": dates[i][1],
             }
-            for year in years
+            for i in range(len(years))
         ],
     )
 
-    # TODO: Supplement with commit times
-
     repos = set()
-    for events_year in events:
+    for events_year in all_events:
         for repo in events_year:
             repos.add(repo)
     repos = list(repos)
+
+    commit_times = gather(
+        funcs=[get_all_commit_times for _ in repos],
+        args_dicts=[
+            {
+                "user_id": user_id,
+                "name_with_owner": repo,
+                "start_date": dates[0][0],  # first start
+                "end_date": dates[-1][1],  # last end
+            }
+            for repo in repos
+        ],
+        max_threads=100,
+    )
+
+    commit_times_dict: Dict[str, List[datetime]] = {}
+    for repo, item in zip(repos, commit_times):
+        commit_times_dict[repo] = item
 
     def get_stats():
         return {
@@ -163,42 +229,56 @@ def get_contributions(
                 total_stats["contribs_count"] += day.count
                 total_stats["other_count"] += day.count
 
-    for events_year in events:
+    def update(date_str: str, repo: str, event: str, count: int):
+        # update global counts for this event
+        total[date_str]["stats"][event + "_count"] += count
+        total_stats[event + "_count"] += count
+        # update repo counts for this event
+        repositories[repo][date_str]["stats"][event + "_count"] += count
+        repo_stats[repo][event + "_count"] += count
+        # update total other stats by subtracting
+        total[date_str]["stats"]["other_count"] -= count
+        total_stats["other_count"] -= count
+        # update repo total stats by adding
+        repositories[repo][date_str]["stats"]["contribs_count"] += count
+        repo_stats[repo]["contribs_count"] += count
+
+    for events_year in all_events:
         for repo, repo_events in events_year.items():
             for event_type in ["commits", "issues", "prs", "reviews", "repos"]:
-                for event in repo_events[event_type]:
-                    datetime_obj = event.occurred_at.astimezone(eastern)
+                events = sorted(repo_events[event_type], key=lambda x: x.occurred_at)
+                for event in events:
+                    datetime_obj = event.occurred_at.astimezone(tz)
                     date_str = str(datetime_obj.date())
+                    repositories[repo][date_str]["date"] = datetime_obj.date()
                     if isinstance(event, RawEventsCommit):
-                        total[date_str]["stats"]["commits_count"] += event.count
-                        total_stats["commits_count"] += event.count
-                        repositories[repo][date_str]["stats"][
-                            "commits_count"
-                        ] += event.count
-                        repo_stats[repo]["commits_count"] += event.count
-                        total[date_str]["stats"]["other_count"] -= event.count
-                        total_stats["other_count"] -= event.count
-                        repositories[repo][date_str]["stats"][
-                            "contribs_count"
-                        ] += event.count
-                        repo_stats[repo]["contribs_count"] += event.count
+                        # get timestamps
+                        timestamps = []
+                        for _ in range(event.count):
+                            if len(commit_times_dict[repo]) > 0:
+                                raw_time = commit_times_dict[repo][0]
+                                time = pytz.utc.localize(raw_time).astimezone(tz)
+                                if (str(time.date())) == date_str:
+                                    commit_times_dict[repo].pop(0)
+                                    timestamps.append(time)
+
+                        # record timestamps
+                        total[date_str]["lists"]["commits"].extend(timestamps)
+                        repositories[repo][date_str]["lists"]["commits"].extend(
+                            timestamps
+                        )
+
+                        # update stats
+                        update(date_str, repo, "commits", event.count)
                     else:
-                        total[date_str]["stats"][event_type + "_count"] += 1
-                        total_stats[event_type + "_count"] += 1
-                        repositories[repo][date_str]["stats"][
-                            event_type + "_count"
-                        ] += 1
-                        repo_stats[repo][event_type + "_count"] += 1
+                        # update stats
+                        update(date_str, repo, event_type, 1)
+
+                        # record timestamps
                         total[date_str]["lists"][event_type].append(datetime_obj)
                         repositories[repo][date_str]["lists"][event_type].append(
                             datetime_obj
                         )
-                        total[date_str]["stats"]["other_count"] -= 1
-                        total_stats["other_count"] -= 1
-                        repositories[repo][date_str]["stats"]["contribs_count"] += 1
-                        repo_stats[repo]["contribs_count"] += 1
-
-                    repositories[repo][date_str]["date"] = datetime_obj.date()
 
     total_list = list(total.values())
     repositories_list = {
