5b6499d678e1630f1c893554bd8b9fbc613c48f8
Abhijit Gupta <avgupta456@gmail.com>
2021-02-14 11:08:48 -0500

    use react router

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +7,13 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,7 +31,9 @@ export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
+    <Router>
     <div className={classes.root}>
+      
       <AppBar position="static">
         <Toolbar>
           {/*
@@ -34,10 +44,29 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             GitHub Trends
           </Typography>
-          <Button color="inherit">User</Button>
-          <Button color="inherit">Octoverse</Button>
+          <Button color="inherit" component={Link} to={"/user"}>User</Button>
+          <Button color="inherit" component={Link} to={"/octoverse"}>Octoverse</Button>
         </Toolbar>
       </AppBar>
     </div>
+
+<Switch>
+<Route path="/user">
+  <User />
+</Route>
+<Route path="/octoverse">
+  <Octoverse />
+</Route>
+</Switch>
+
+</Router>
   );
+}
+
+function User() {
+  return <h2>User</h2>;
+}
+
+function Octoverse() {
+  return <h2>Octoverse</h2>;
 }
