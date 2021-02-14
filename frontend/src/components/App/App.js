30b87f9e25ff460f145b428ada922a0595be7aca
Abhijit Gupta <avgupta456@gmail.com>
2021-02-14 08:52:05 -0500

    add material-ui and roboto

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import "@fontsource/roboto"
+
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
