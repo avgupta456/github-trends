78a6e3bb5d7745d72cf4ecb0d3c6de19fdd70df2
Abhijit Gupta <avgupta456@gmail.com>
2021-02-14 10:58:39 -0500

    Create AppBar

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,9 +1,43 @@
-function App() {
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Test
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/*
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          */}
+          <Typography variant="h6" className={classes.title}>
+            GitHub Trends
+          </Typography>
+          <Button color="inherit">User</Button>
+          <Button color="inherit">Octoverse</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
-
-export default App;
