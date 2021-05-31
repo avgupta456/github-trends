0540b93a58e590d60be9fb7c85de9459cba0ba05
Abhijit Gupta <avgupta456@gmail.com>
2021-05-30 23:13:15 -0400

    use datastore

--- backend/endpoints/github_auth.py
+++ backend/endpoints/github_auth.py
@@ -5,6 +5,7 @@ import requests
 
 from constants import OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REDIRECT_URI
 from external.github_auth.auth import get_unknown_user
+from external.google_datastore.datastore import set_access_token
 
 
 s = requests.session()
@@ -26,14 +27,12 @@ def get_access_token(code: str) -> Any:
         "redirect_uri": OAUTH_REDIRECT_URI,
     }
 
-    print(params)
-
     r = s.post("https://github.com/login/oauth/access_token", params=params)  # type: ignore
 
     if r.status_code == 200:
         access_token = r.text.split("&")[0].split("=")[1]
         user_id = get_unknown_user(access_token)
-        print(user_id, access_token)
+        set_access_token(user_id, access_token)
         print("OAuth API", datetime.now() - start)
         return user_id
 
