82a813c1f2f13be0d3de17a9eb2656e166bcdb70
Abhijit Gupta <avgupta456@gmail.com>
2021-09-21 10:06:04 -0400

    Update Login.js

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,70 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { Redirect } from 'react-router-dom';
-
-// import GithubIcon from 'mdi-react/GithubIcon';
+import GithubIcon from 'mdi-react/GithubIcon';
 
 import { Button } from '../../components';
-import { getAccessToken } from '../../api';
-import {
-  login as _login,
-  logout as _logout,
-} from '../../redux/actions/userActions';
-import { CLIENT_ID, REDIRECT_URI } from '../../constants';
-
-const GitHubAuthURL = `https://github.com/login/oauth/authorize?scope=user,repo&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+import { GITHUB_AUTH_URL } from '../../constants';
 
 const LoginScreen = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   // const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const userId = useSelector((state) => state.user.userId);
 
-  const dispatch = useDispatch();
-
-  const login = (newUserId) => dispatch(_login(newUserId));
-  const logout = () => dispatch(_logout());
-
   console.log(userId);
 
-  useEffect(async () => {
-    // After requesting Github access, Github redirects back to your app with a code parameter
-    const url = window.location.href;
-    const hasCode = url.includes('?code=');
-
-    // If Github API returns the code parameter
-    if (hasCode) {
-      const newUrl = url.split('?code=');
-      window.history.pushState({}, null, newUrl[0]);
-      setIsLoading(true);
-      const result = await getAccessToken(newUrl[1]);
-      login(result);
-      setIsLoading(false);
-    }
-  }, []);
-
   return (
-    <section className="px-8 py-8">
-      <h1>{`Welcome ${userId}!`}</h1>
-      <span>Super amazing app</span>
-      {!isLoading && (
-        <div className="flex justify-center">
-          <Button className="text-white bg-blue-500 hover:bg-blue-600">
-            <a href={GitHubAuthURL}>
-              {/* <GithubIcon /> */}
-              Login with GitHub
+    <div className="h-full py-8">
+      <div className="h-full flex justify-center items-center">
+        <div className="h-auto w-72 bg-gray-100 border-2 border-gray-300 rounded p-4">
+          <h3 className="text-xl font-semibold text-gray-900">
+            Login with GitHub!
+          </h3>
+          <div className="mt-2 text-xs text-gray-600">
+            Authenticate with your GitHub account to access your statistics. You
+            can optionally give read access to private repositories.
+          </div>
+          <div className="mt-4 flex justify-center">
+            <a href={GITHUB_AUTH_URL} className="inline-flex">
+              <Button className="text-white bg-blue-500 hover:bg-blue-600">
+                <span className="mr-2">Login with GitHub</span>
+                <GithubIcon />
+              </Button>
             </a>
-          </Button>
-          <Button
-            onClick={logout}
-            className="ml-4 text-gray-700 bg-gray-100 hover:bg-gray-200"
-          >
-            <span>Logout</span>
-          </Button>
+          </div>
         </div>
-      )}
-    </section>
+      </div>
+    </div>
   );
 };
 
