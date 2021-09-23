350630554158c7806a22aa5949695efb18853c8b
Abhijit Gupta <avgupta456@gmail.com>
2021-09-23 02:07:29 -0400

    prepare prod/dev

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,9 @@
+export const PROD = process.env.REACT_APP_PROD;
+
 export const USE_LOGGER = true;
 
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const REDIRECT_URI = 'http://localhost:3000/user';
+export const REDIRECT_URI = PROD
+  ? 'https://githubtrends.io/user'
+  : 'http://localhost:3000/user';
 export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?scope=user,repo&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
