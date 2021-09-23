1a0ce23434e7c38a86304a5fa803f72b12532985
Abhijit Gupta <avgupta456@gmail.com>
2021-09-23 19:01:12 -0400

    copy link

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
+import CopyIcon from 'mdi-react/ContentCopyIcon';
+
+import { classnames } from '../utils';
+
+import './card.css';
+
 const Card = ({ title, description, imageSrc }) => {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copied) setCopied(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [copied]);
+
   return (
     <div className="p-2 md:w-1/3 sm:mb-0 mb-6">
       <div className="p-4 rounded-lg bg-gray-50 hover:bg-gray-100">
-        <img
-          alt="content"
-          className="object-cover h-full w-full"
-          src={imageSrc}
-        />
+        <div className={classnames('card_container', 'relative')}>
+          <img
+            alt="content"
+            className={classnames('image', 'object-cover h-full w-full')}
+            src={imageSrc}
+          />
+          <button
+            type="button"
+            className={classnames(
+              'overlay',
+              'h-full w-full text-blue-500 flex justify-center items-center',
+            )}
+            onClick={() => {
+              navigator.clipboard.writeText(
+                `[![GitHub Trends SVG](${imageSrc})](https://githubtrends.io)`,
+              );
+              setCopied(true);
+            }}
+          >
+            {copied ? (
+              'Copied Link!'
+            ) : (
+              <>
+                Copy Link
+                <CopyIcon className="ml-1" />
+              </>
+            )}
+          </button>
+        </div>
         <h2 className="text-xl font-medium title-font text-gray-900 mt-5">
           {title}
         </h2>
