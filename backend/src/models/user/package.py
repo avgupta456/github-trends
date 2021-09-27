6e71b3c637b0cfbcaf32a2264edea49cfea8fe34
Abhijit Gupta <avgupta456@gmail.com>
2021-09-27 16:23:47 -0400

    disable follows

--- backend/src/models/user/package.py
+++ backend/src/models/user/package.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 
 from src.models.user.contribs import UserContributions
-from src.models.user.follows import UserFollows
+
+# from src.models.user.follows import UserFollows
 
 
 class UserPackage(BaseModel):
     contribs: UserContributions
-    follows: UserFollows
+    # follows: UserFollows
