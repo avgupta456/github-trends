8819f0ed34061babd1e6290be349a9344219023b
Abhijit Gupta <avgupta456@gmail.com>
2021-10-28 19:19:51 -0400

    update data when upgrading

--- backend/src/db/user/functions.py
+++ backend/src/db/user/functions.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from datetime import datetime, date
 from typing import Any, Dict, Optional
+from src.external.pubsub.templates import publish_to_topic
 
 from src.models.user.package import UserPackage
 from src.db.mongodb import USERS
 from src.db.user.compression import compress
+from src.helper.utils import date_to_datetime
 
 
 async def login_user(
@@ -17,7 +19,15 @@ async def login_user(
 
     # update private access without downgrading
     curr_private_access = curr_user.get("private_access", False) if curr_user else False
-    raw_user["private_access"] = curr_private_access or private_access
+    new_private_access = curr_private_access or private_access
+
+    raw_user["private_access"] = new_private_access
+    if new_private_access != curr_private_access:
+        # queues data query after cache expires
+        raw_user["last_updated"] = date_to_datetime(date(1970, 1, 1))
+
+        # triggers data query to fetch private commits
+        publish_to_topic("user", {"user_id": user_id, "access_token": access_token})
 
     if curr_user is None:
         raw_user["last_updated"] = datetime.now()
