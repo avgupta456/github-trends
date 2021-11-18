53feead0c25bc2a69467713208137df9adecb14e
Abhijit Gupta <avgupta456@gmail.com>
2021-11-18 01:21:17 -0500

    retries on wrapped api call

--- frontend/src/api/wrapped.js
+++ frontend/src/api/wrapped.js
@@ -1,17 +1,26 @@
+/* eslint-disable no-return-await */
+
 import axios from 'axios';
 
 import { BACKEND_URL } from '../constants';
 
 const URL_PREFIX = `${BACKEND_URL}/wrapped`;
 
-const getWrapped = async (userId, year) => {
+const getWrapped = async (userId, year, retries = 0) => {
   try {
     const fullUrl = `${URL_PREFIX}/${userId}?year=${year}`;
     const result = await axios.get(fullUrl);
-    return result.data.data;
+    const output = result.data.data;
+    if (output === null || output === undefined) {
+      throw new Error('No data');
+    }
+    return output;
   } catch (error) {
+    if (retries < 3) {
+      return await getWrapped(userId, year, retries + 1);
+    }
     console.error(error);
-    return '';
+    return null;
   }
 };
 
