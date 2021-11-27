94c6c9a09a32e8c4dfc5993edbc86098a0e710b1
Abhijit Gupta <avgupta456@gmail.com>
2021-11-27 15:44:21 -0500

    Add to landing page

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,14 +1,16 @@
-/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-one-expression-per-line */
+
 import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 import { FaGithub as GithubIcon } from 'react-icons/fa';
 
-import { Button } from '../../components';
+import { Button, Preview } from '../../components';
 import { classnames } from '../../utils';
 
 import mockup from '../../assets/mockup.png';
+import wrapped from '../../assets/wrapped.png';
 
 const LandingScreen = () => {
   const userId = useSelector((state) => state.user.userId);
@@ -17,7 +19,7 @@ const LandingScreen = () => {
 
   return (
     <section>
-      <div className="bg-blue-500 text-gray-100 w-full flex flex-wrap py-16 px-4">
+      <div className="bg-blue-500 text-gray-100 w-full flex flex-wrap py-24 px-4">
         <div className="w-1/2 flex flex-col justify-center p-4">
           <div className="w-full text-center text-5xl font-bold mb-6">
             Discover and share code contribution insights
@@ -59,8 +61,101 @@ const LandingScreen = () => {
           <img src={mockup} alt="preview" />
         </div>
       </div>
-      <div className="bg-gray-100 text-gray-700 w-full flex flex-wrap py-16 px-4">
-        <div>Test</div>
+      <div className="bg-gray-200 text-gray-700 w-full flex flex-wrap items-center py-4 px-4">
+        <div className="w-1/2 p-8 flex justify-center">
+          <Preview />
+        </div>
+        <div className="w-1/2 p-8 flex flex-col">
+          <h1 className="text-4xl text-gray-900 font-medium mb-12">
+            Display your GitHub stats
+            <div>with embeddable cards</div>
+          </h1>
+          <p className="text-lg font-bold">1. Comprehensive</p>
+          <p>
+            GitHub Trends counts each individual commit, across all your
+            open-source contributions. We surface line of code metrics by
+            repository and languages.
+          </p>
+          <br />
+          <p className="text-lg font-bold">2. Customizable</p>
+          <p>
+            Using the online dashboard, easily modify the time range, include
+            private commits, and choose your display theme.
+          </p>
+          <br />
+          <p className="text-lg font-bold">3. Shareable</p>
+          <p>
+            Share your GitHub Trends cards as a PNG on Twitter, or as a dynamic
+            embeddable image on your GitHub profile or personal website.
+          </p>
+          <br />
+          <div>
+            <Link to={isAuthenticated ? '/user' : '/demo'} className="w-auto">
+              <Button className="my-4 mr-4 w-auto justify-center text-white text-xl bg-gray-700 hover:bg-gray-800">
+                {isAuthenticated ? 'Visit Dashboard' : 'Try the Demo'}
+              </Button>
+            </Link>
+            {!isAuthenticated && (
+              <Link to="/signup" className="w-auto">
+                <Button className="my-4 mr-4 w-auto justify-center text-white text-xl bg-blue-500 hover:bg-blue-600">
+                  Get Started
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="text-gray-700 w-full flex flex-wrap items-center py-4 px-4">
+        <div className="w-1/2 p-8 flex flex-col">
+          <h1 className="text-4xl text-gray-900 font-medium mb-12">
+            Reflect on your year
+            <div>
+              with <strong>GitHub Wrapped</strong>
+            </div>
+          </h1>
+          <p className="text-lg font-bold">1. Detailed</p>
+          <p>
+            GitHub Wrapped provides a breakdown of your contributions by date,
+            by date, time, repository, and language. Over 20 stats are
+            displayed.
+          </p>
+          <br />
+          <p className="text-lg font-bold">2. Visual</p>
+          <p>
+            Understand your coding contributions like never before with an
+            interactive calendar, bar charts, pie charts, swarm graphs, and
+            more.
+          </p>
+          <br />
+          <p className="text-lg font-bold">3. Public</p>
+          <p>
+            Share your GitHub Wrapped link with your friends and colleagues and
+            take a look at their contributions too.{' '}
+            <strong>No account required</strong>, although rate limiting may
+            apply.
+          </p>
+          <br />
+          <div>
+            <Link to="/wrapped/avgupta456" className="w-auto">
+              <Button className="my-4 mr-4 w-auto justify-center text-white text-xl bg-gray-700 hover:bg-gray-800">
+                Example
+              </Button>
+            </Link>
+            <Link
+              to={isAuthenticated ? `/wrapped/${userId}` : '/wrapped'}
+              className="w-auto"
+            >
+              <Button className="my-4 mr-4 w-auto justify-center text-white text-xl bg-blue-500 hover:bg-blue-600">
+                Get your Wrapped
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="w-1/2 p-8">
+          <div className="flex justify-center">
+            <img src={wrapped} alt="preview" />
+          </div>
+        </div>
       </div>
     </section>
   );
