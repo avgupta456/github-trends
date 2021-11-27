85b3faea1097499bdadcaf726158ddf85d32e41b
Abhijit Gupta <avgupta456@gmail.com>
2021-11-27 15:48:01 -0500

    update auth pages

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 
 import { FaGithub as GithubIcon } from 'react-icons/fa';
 
-import { Button, Preview } from '../../components';
+import { Button } from '../../components';
 import { GITHUB_PUBLIC_AUTH_URL } from '../../constants';
 import { classnames } from '../../utils';
+
+import mockup from '../../assets/mockup.png';
 
 const LoginScreen = () => {
   // eslint-disable-next-line no-unused-vars
@@ -15,12 +17,12 @@ const LoginScreen = () => {
 
   return (
     <div className="h-full flex flex-wrap">
-      <div className="hidden lg:block lg:w-1/2 lg:p-8 lg:my-auto">
+      <div className="hidden lg:block lg:w-3/5 lg:p-8 lg:my-auto">
         <div className="w-4/5 mx-auto">
-          <Preview />
+          <img src={mockup} alt="preview" />
         </div>
       </div>
-      <div className="w-full h-full lg:w-1/2 flex lg:flex-col">
+      <div className="w-full h-full lg:w-2/5 flex lg:flex-col lg:p-8">
         <div
           className={classnames(
             'bg-gray-100 rounded-sm w-full h-full m-auto p-8 shadow',
--- frontend/src/pages/Auth/SignUp.js
+++ frontend/src/pages/Auth/SignUp.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom';
 
 import { FaGithub as GithubIcon } from 'react-icons/fa';
 
-import { Button, Preview } from '../../components';
+import { Button } from '../../components';
 import {
   GITHUB_PUBLIC_AUTH_URL,
   GITHUB_PRIVATE_AUTH_URL,
 } from '../../constants';
 import { classnames } from '../../utils';
+
+import mockup from '../../assets/mockup.png';
 
 const SignUpScreen = () => {
   // eslint-disable-next-line no-unused-vars
@@ -18,12 +20,12 @@ const SignUpScreen = () => {
 
   return (
     <div className="h-full flex flex-wrap">
-      <div className="hidden lg:block lg:w-1/2 lg:p-8 lg:my-auto">
+      <div className="hidden lg:block lg:w-3/5 lg:p-8 lg:my-auto">
         <div className="w-4/5 mx-auto">
-          <Preview />
+          <img src={mockup} alt="preview" />
         </div>
       </div>
-      <div className="w-full h-full lg:w-1/2 flex lg:flex-col">
+      <div className="w-full h-full lg:w-2/5 flex lg:flex-col lg:p-8">
         <div
           className={classnames(
             'bg-gray-100 rounded-sm w-full h-full m-auto p-8 shadow',
