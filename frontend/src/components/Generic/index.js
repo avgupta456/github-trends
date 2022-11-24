12c864be6f2fff650dd6793ccfd3ef27e0d7456a
Abhijit Gupta <avgupta456@gmail.com>
2022-11-24 15:39:49 -0500

    Save Wrapped as Image (#199)

--- frontend/src/components/Generic/index.js
+++ frontend/src/components/Generic/index.js
@@ -1,6 +1,5 @@
 import Button from './Button';
 import Checkbox from './Checkbox';
-import FloatingIcon from './FloatingIcon';
 import Input from './Input';
 
-export { Button, Checkbox, FloatingIcon, Input };
+export { Button, Checkbox, Input };
